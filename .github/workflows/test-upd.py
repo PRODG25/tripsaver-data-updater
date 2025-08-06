7ba1411f3d7c51b6e1c2c26c8e2620fdc436aa74
PRODG25 <129597427+PRODG25@users.noreply.github.com>
2025-08-06 15:24:36 +0200

    Update test-upd.py

--- .github/workflows/test-upd.py
+++ .github/workflows/test-upd.py
@@ -147,7 +147,7 @@ df = df.merge(stats, on=['Route', 'Month'], how='left')
 df['z_score'] = (df['Total Price'] - df['AvgPrice']) / df['StdDev']
 
 # Filter best deals: at least 1 std dev below avg
-#best_deals = df[df['z_score'] <= -1].copy()
+best_deals = df[df['z_score'] <= -0.5].copy()
 
 # Sort by best deals first
 df = df.sort_values(by='Total Price')
