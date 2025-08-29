c1107f3c3cb1ec57badbcab0aea9f02f3284a823
PRODG25 <129597427+PRODG25@users.noreply.github.com>
2025-08-29 21:26:42 +0200

    Update test-upd.py

--- .github/workflows/test-upd.py
+++ .github/workflows/test-upd.py
@@ -68,7 +68,7 @@ valid_trips['route'] = valid_trips['DepartureCity'] + ' - ' + valid_trips['Arriv
 
 # Group by route and departure month, get top 10% cheapest flights per group
 def top_10_percent(group):
-    cutoff = int(len(group) * 0.3)
+    cutoff = int(len(group) * 0.4)
     if cutoff == 0:
         cutoff = 1
     return group.nsmallest(cutoff, 'total_price')
