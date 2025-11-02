0a69fc7df500144f0e73b011f14e5036eee4d235
PRODG25 <129597427+PRODG25@users.noreply.github.com>
2025-11-02 01:27:22 +0100

    Update test-upd.py

--- .github/workflows/test-upd.py
+++ .github/workflows/test-upd.py
@@ -170,42 +170,21 @@ def create_trip_link(row):
             f"{destination}/"
             f"{return_date}/"
             f"{return_airport}/"
-            f"?adultsv2=1"
-            f"&cabinclass=economy"
-            f"&childrenv2="
-            f"&associateid={ASSOCIATE_ID}"
-            f"&utm_medium=affiliate"
-            f"&utm_source=6439681-Trip%20Saver"
-            f"&utm_campaign="
-            f"&campaign_id={MARKER}"
-            f"&utm_content=Online%20Tracking%20Link"
+            f"?adultsv2=1&cabinclass=economy&market=pl&locale=pl-pl&currency=pln&referralServiceVersion=meeseeks-referral-service-v1&utm_term=HelloWorld&irclickid=_sdn6b6xn29cfri9kj3whlzkrkv22zz1u23x2rfc000&associateid=AFF_TRA_19354_00001&utm_medium=affiliate&utm_source=6439681-Trip%20Saver&utm_campaign=&campaign_id=6439681&utm_content=Flight%20Search%20Widget&adid=1103262&click_timestamp=1762043153649&irmweb=&irgwc=1&afsrc=1"
         )
     else:
         # Round trip format uses YYMMDD (via format_skyscanner_date)
         departure_date = format_skyscanner_date(row['Departure Date'])
         return_date = format_skyscanner_date(row['Return Date'])
         
-        return (
-            f"https://www.skyscanner.pl/transport/loty/"
-            f"{departure}/"
-            f"{destination}/"
-            f"{departure_date}/"
-            f"{return_date}/"
-            f"?adultsv2=1"
-            f"&cabinclass=economy"
-            f"&childrenv2="
-            f"&rtn=1"
-            f"&preferdirects=false"
-            f"&outboundaltsenabled=false"
-            f"&inboundaltsenabled=false"
-            f"&associateid={ASSOCIATE_ID}"
-            f"&utm_medium=affiliate"
-            f"&utm_source=6439681-Trip%20Saver"
-            f"&utm_campaign="
-            f"&campaign_id={MARKER}"
-            f"&utm_content=Online%20Tracking%20Link"
-        )
 
+        return (f"https://www.skyscanner.pl/transport/loty/"
+                f"{departure}/"
+                f"{destination}/"
+                f"{departure_date}/"
+                f"{return_date}/"
+                f"?adultsv2=1&cabinclass=economy&market=pl&locale=pl-pl&currency=pln&referralServiceVersion=meeseeks-referral-service-v1&utm_term=HelloWorld&associateid=AFF_TRA_19354_00001&utm_medium=affiliate&utm_source=6439681-Trip%20Saver&utm_campaign=&campaign_id=6439681&utm_content=Flight%20Search%20Widget&adid=1103262&irmweb=&irgwc=1&afsrc=1"
+               )
 final_df['Round_Trip_Link'] = final_df.apply(create_trip_link, axis=1)
 
 
